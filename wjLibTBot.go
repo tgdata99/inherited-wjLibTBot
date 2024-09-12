801de5b42437a13cdcecf5992e3669043d77e655
tgdata99 <info@tgdata.serv00.net>
2024-09-12 05:57:17 +0000

    11

--- wjLibTBot.go
+++ wjLibTBot.go
@@ -1,6 +1,6 @@
 package wjLibTBot
 import innerTelegramBotter "github.com/ancestortelegram/wjLibTBot/internal"
-//import innerTelegramBotter "github.com/ancestortelegram/wjLibTBot/internel"
+//import innerTelegramBotter "github.com/ancestortelegram/wjLibTBot/internal"
 
 func WJLTB_NewBotter(id int) (IWJTelegramBotAPI, error) {
 	return innerTelegramBotter.newTelegramBotApi(id)
